7ef2eaf8cac5bdfc966a2d5e1df73d403c73bd09
Enrico 'Henryx' Bianchi <henryx_b@yahoo.it>
2010-12-09 00:50:17 +0100

    First implementation of SSH transport protocol

---
+++ src/protocols.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2010 Enrico Bianchi (enrico.bianchi@gmail.com)
+Project       BackupSYNC
+Description   A backup system
+License       GPL version 2 (see GPL.txt for details)
+"""
+
+import paramiko
+
+class SSH(object):
+    _cfg = None
+    _client = None
+    
+    def __init__(self, cfg):
+        self._cfg = cfg
+
+    def connect(self, section):
+        self._client = paramiko.SSHClient()
+        self._client.load_system_host_keys()
+        self._client.connect(hostname=self._cfg.get(section, 'remote_host'),
+                             port=self._cfg.get(section, 'remote_port'),
+                             username=self._cfg.get(section, 'remote_user'),
+                             password=self._cfg.get(section, 'remote_password')
+                            )
+
+    def get_file(self, remote, local):
+        sftp = self._client.open_sftp()
+        sftp.get(remote, local)
+        return sftp.stat(remote)
+
+    def send_cmd(self, cmd):
+        stdin, stdout, stderr = self._client.exec_command(cmd)
+
+        error = 0
+        errstr = []
+        for value in stderr:
+            error = error + 1
+            errstr.append(value)
+
+        if error > 0:
+            for value in errstr:
+                print value
+                
+            return False
+        else:
+            return True
