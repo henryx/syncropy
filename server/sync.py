b18851a8d946f9db990f5ee3d18c0954324893f5
Enrico Bianchi <enrico.bianchi@ymail.com>
2014-03-11 00:01:46 +0100

    Server: Code cleanup using context managers

--- server/sync.py
+++ server/sync.py
@@ -31,6 +31,7 @@ import os
 import pickle
 import socket
 import ssl
+from contextlib import closing
 
 import storage
 
@@ -51,6 +52,9 @@ def fs_get_conn(cfg, section):
         conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     logger.debug(section + ": Socket created")
 
+    conn.connect((cfg[section]["host"], int(cfg[section]["port"])))
+    logger.debug(section + ": Socket connected")
+
     return conn
 
 def fs_get_metadata(cfg, section):
@@ -64,14 +68,10 @@ def fs_get_metadata(cfg, section):
         }
     }
 
-    conn = fs_get_conn(cfg, section["name"])
+    with closing(fs_get_conn(cfg, section["name"])) as conn, storage.Database(cfg) as dbs:
+        conn.send(json.dumps(cmdlist).encode("utf-8"))
+        logger.debug(section["name"] + ": JSON command list sended")
 
-    conn.connect((cfg[section["name"]]["host"], int(cfg[section["name"]]["port"])))
-    logger.debug(section["name"] + ": Socket connected")
-    conn.send(json.dumps(cmdlist).encode("utf-8"))
-    logger.debug(section["name"] + ": JSON command list sended")
-
-    with storage.Database(cfg) as dbs:
         f = conn.makefile()
         for data in f:
             response = json.loads(data)
@@ -79,8 +79,8 @@ def fs_get_metadata(cfg, section):
             if response["attrs"]["type"] == "directory":
                 storage.fs_create_dir(cfg, section, response["name"])
 
-    logger.debug(section["name"] + ": JSON list readed")
-    conn.close()
+        logger.debug(section["name"] + ": JSON list readed")
+
 
 def fs_get_data(cfg, section):
     if (section["dataset"] - 1) == 0:
@@ -94,10 +94,8 @@ def fs_get_data(cfg, section):
                 os.link(os.sep.join([storage.fs_compute_destination(cfg, section, True), item[0]]),
                         os.sep.join([storage.fs_compute_destination(cfg, section, False), item[0]]))
             else:
-                conn = fs_get_conn(cfg, section["name"])
-                conn.connect((cfg.get(section["name"], "host"), cfg.getint(section["name"], "port")))
-                storage.fs_save_file(cfg, section, item[0], conn)
-                conn.close()
+                with closing(fs_get_conn(cfg, section["name"])) as conn:
+                    storage.fs_save_file(cfg, section, item[0], conn)
 
 def fs_start(conf, process):
     cfg = pickle.loads(conf)
@@ -115,10 +113,8 @@ def fs_start(conf, process):
                     "value": cfg[section["name"]]["pre_command"]
                 }
             }
-            conn = fs_get_conn(cfg, section["name"])
-            conn.connect((cfg.get(section["name"], "host"), cfg.getint(section["name"], "port")))
-            conn.send(json.dumps(execmd).encode("utf-8"))
-            conn.close()
+            with closing(fs_get_conn(cfg, section["name"])) as conn:
+                conn.send(json.dumps(execmd).encode("utf-8"))
 
     with storage.Database(cfg) as dbs:
         storage.db_del_dataset(dbs, section)
@@ -139,9 +135,7 @@ def fs_start(conf, process):
                     "value": cfg[section["name"]]["post_command"]
                 }
             }
-            conn = fs_get_conn(cfg, section["name"])
-            conn.connect((cfg.get(section["name"], "host"), cfg.getint(section["name"], "port")))
-            conn.send(json.dumps(execmd).encode("utf-8"))
-            conn.close()
+            with closing(fs_get_conn(cfg, section["name"])) as conn:
+                conn.send(json.dumps(execmd).encode("utf-8"))
 
     logger.debug(section["name"] + ": Sync done")
